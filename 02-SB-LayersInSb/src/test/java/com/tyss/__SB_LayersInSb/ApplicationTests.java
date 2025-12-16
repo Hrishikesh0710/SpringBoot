f1b3e3e0e387cb3e278fb73530d84e6d2f977814
Hrishikesh0710 <hrishipan1081@gmail.com>
2025-12-16 19:25:34 +0530

    saved

---
+++ 02-SB-LayersInSb/src/test/java/com/tyss/__SB_LayersInSb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyss.__SB_LayersInSb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
