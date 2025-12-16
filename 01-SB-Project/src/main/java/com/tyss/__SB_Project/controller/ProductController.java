077ad456bc6d870c36aa4a1070708fb4947286fa
Hrishikesh0710 <hrishipan1081@gmail.com>
2025-12-16 19:25:21 +0530

    saved

--- 01-SB-Project/src/main/java/com/tyss/__SB_Project/controller/ProductController.java
+++ 01-SB-Project/src/main/java/com/tyss/__SB_Project/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +37,7 @@ public class ProductController {
 	
 	@GetMapping("/FetchById")
 	public Product fetchById(@RequestParam Integer pid) {
-		Optional<Product> opt = productRepository.findById(pid);
+		 Optional<Product> opt = productRepository.findById(pid);
 
 		if (opt.isPresent()) {
 			Product product = opt.get();
@@ -52,10 +53,34 @@ public class ProductController {
 		//return productRepository.findById(pid).orElseThrow(() -> new RuntimeException("Product not found"));
 	}
 	
-	@DeleteMapping("/{pid}")
+	@DeleteMapping("/delete/{pid}")
 	public String delete(@PathVariable Integer pid) {
 	    productRepository.deleteById(pid);
 	    return "Deleted successfully";
+	}
+	
+	
+	@PutMapping("/update/{pid}")
+	public Product updateProduct(@PathVariable Integer pid,@RequestBody Product product) {
+		
+		Optional<Product> opt = productRepository.findById(pid);
+		if (opt.isPresent()) {
+			Product dbProduct = opt.get();
+
+			if (product.getName() != null)
+				dbProduct.setName(product.getName());
+			if (product.getPrice() > 0)
+				dbProduct.setPrice(product.getPrice());
+			if (product.getDescription() != null)
+				dbProduct.setDescription(product.getDescription());
+			if (product.getColor() != null)
+				dbProduct.setColor(product.getColor());
+
+			return productRepository.save(dbProduct);
+
+		}
+		return null;
+		
 	}
 
 
